2b6bde15ada38a60efa0421f41e5ade4e56ef839
Aileron <purgatory720@gmail.com>
2020-02-12 10:31:16 +1000

    Add initial package and class

---
+++ src/com/hololiveresistance/matsuriPlugin/MatsuriPlugin.java
@@ -0,0 +1,16 @@
+package com.hololiveresistance.matsuriPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MatsuriPlugin extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    	getLogger().info("WASSHOI, WORLD!");
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+
+}
