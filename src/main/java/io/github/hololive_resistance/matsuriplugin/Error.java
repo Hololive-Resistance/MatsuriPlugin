25ad6f12b186a0a1f23d7060acda1b8e2fbe1720
Pigu-A <supa7764@gmail.com>
2020-02-12 21:33:23 +0700

    Use Maven and remove local libraries

---
+++ src/main/java/io/github/hololive_resistance/matsuriplugin/Error.java
@@ -0,0 +1,12 @@
+package io.github.hololive_resistance.matsuriplugin;
+
+import java.util.logging.Level;
+
+public class Error {
+    public static void execute(MatsuriPlugin plugin, Exception ex){
+        plugin.getLogger().log(Level.SEVERE, "Couldn't execute MySQL statement: ", ex);
+    }
+    public static void close(MatsuriPlugin plugin, Exception ex){
+        plugin.getLogger().log(Level.SEVERE, "Failed to close MySQL connection: ", ex);
+    }
+}
